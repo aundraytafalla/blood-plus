15c80c5b226b53900ca9ee30fa7eda4338c85c34
CrushieT <aundraytafalla@gmail.com>
2026-02-22 16:03:33 +0800

    Created the repo

---
+++ src/main/java/com/prc/blood_plus/BloodPlusApplication.java
@@ -0,0 +1,13 @@
+package com.prc.blood_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prc/blood_plus/BloodPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prc.blood_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
